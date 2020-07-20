db5db61317267467b76da938f29d16eeda9167e5
zhuyi <zhu_w_yi@163.com>
2020-07-20 17:54:42 +0800

    首页

--- flutter_zhuyi_test/lib/pages/home_page.dart
+++ flutter_zhuyi_test/lib/pages/home_page.dart
@@ -1,19 +1,177 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_zhuyi_test/dao/home_dao.dart';
+import 'package:flutter_zhuyi_test/model/common_model.dart';
+import 'package:flutter_zhuyi_test/model/config_model.dart';
+import 'package:flutter_zhuyi_test/model/grid_nav_model.dart';
+import 'package:flutter_zhuyi_test/model/home_model.dart';
+import 'package:flutter_zhuyi_test/model/sales_box_model.dart';
+import 'package:flutter_zhuyi_test/widget/loading_container.dart';
+import 'package:flutter_zhuyi_test/widget/local_nav.dart';
+import 'package:flutter_zhuyi_test/widget/webview.dart';
+import 'package:flutter_zhuyi_test/util/navigator_util.dart';
+const APPBAR_SCROLL_OFFSET = 100;
+const SEARCH_BAR_DEFAULT_TEXT = '网红打卡地 景点 酒店 美食';
 
 class HomePage extends StatefulWidget{
+  static ConfigModel configModel;
+
   @override
   _HomePageState createState() =>_HomePageState();
 
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
+  double appBarAlpha = 0;
+  List<CommonModel> localNavList = [];
+  List<CommonModel> bannerList = [];
+  List<CommonModel> subNavList = [];
+  GridNavModel gridNavModel;
+  SalesBoxModel salesBoxModel;
+  bool _loading = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _handleRefresh();
+    Future.delayed(Duration(milliseconds: 600), () {
+//      FlutterSplashScreen.hide();
+    });
+  }
+
+  _onScroll(offset) {
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print(appBarAlpha);
+  }
+
+  Future<Null> _handleRefresh() async {
+    try {
+      HomeModel model = await HomeDao.fetch();
+      setState(() {
+        HomePage.configModel = model.config;
+        localNavList = model.localNavList;
+        subNavList = model.subNavList;
+        gridNavModel = model.gridNav;
+        salesBoxModel = model.salesBox;
+        bannerList = model.bannerList;
+        _loading = false;
+      });
+    }
+    catch (e) {
+      print(e);
+      setState(() {
+        _loading = false;
+      });
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text(
-        '首页',
+    return Scaffold(
+      backgroundColor: Color(0xfff2f2f2),
+      body: LoadingContainer(isLoading: _loading,
+          child: Stack(
+            children: <Widget>[
+              MediaQuery.removePadding(
+                removeTop: true,
+                context: context,
+                child: RefreshIndicator(
+                  child: NotificationListener(
+                    onNotification: (scrollNotification) {
+                      if (scrollNotification is ScrollUpdateNotification &&
+                          scrollNotification.depth == 0) {
+                        //滚动且是列表滚动的时候
+                        _onScroll(scrollNotification.metrics.pixels);
+                      }
+                    },
+                    child: _listView,
+                  ),
+                  onRefresh: _handleRefresh,
+                ),
+              ),
+              _appBar
+            ],
+          )),
     );
   }
 
+  Widget get _listView {
+    return ListView(
+      children: <Widget>[
+        _banner,
+        Padding(
+            child: LocalNav(localNavList: localNavList),
+            padding: EdgeInsets.fromLTRB(7, 4, 7, 4)),
+      ],
+    );
+  }
+
+  Widget get _appBar {
+    return Column(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              //AppBar渐变遮罩背景
+              colors: [Color(0x66000000), Colors.transparent],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Container(
+            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+            height: 80.0,
+            decoration: BoxDecoration(
+              color: Color.fromARGB((appBarAlpha * 255).toInt(), 255, 255, 255),
+            ),
+          ),
+        ),
+        Container(
+          height: appBarAlpha>0.2?0.5:0,
+          decoration: BoxDecoration(
+          boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 0.5)]),
+        ),
+      ],
+    );
+  }
+
+  Widget get _banner {
+    return Container(
+      height: 160,
+      child: Swiper(
+        itemCount: bannerList.length,
+        autoplay: true,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              CommonModel model = bannerList[index];
+              NavigatorUtil.push(context,
+                  WebView(
+                      url: model.url,
+                      title: model.title,
+                      hideAppBar: model.hideAppBar));
+            },
+            child: Image.network(
+              bannerList[index].icon,
+              fit: BoxFit.fill,
+            ),
+          );
+        },
+        pagination: SwiperPagination(),//显示小圆点
+      ),
+    );
+  }
 }
